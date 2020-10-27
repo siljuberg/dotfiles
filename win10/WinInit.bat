9692cb709b820f183f6baeabf4f1fbfbeae5e9f9
Morten Kristoffer Siljuberg <MSiljuberg@slb.com>
2020-10-27 13:39:07 +0100

    updates

--- win10/WinInit.bat
+++ win10/WinInit.bat
@@ -4,4 +4,10 @@ rem %AppData%\Microsoft\Windows\Start Menu\Programs\Startup
 
 
 REM ************* Set default text editor across Windows ***********
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\txtfile\shell\open\command"  /f /t REG_EXPAND_SZ /d "\"C:\Program Files\Notepad++\notepad++.exe\" \"%%1\""
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\txtfile\shell\open\command" /f /t REG_EXPAND_SZ /d "\"C:\Program Files\Notepad++\notepad++.exe\" \"%%1\""
+
+REM ************* Open Unknown Files in Notepad++ ***********
+REG ADD HKEY_CLASSES_ROOT\Unknown\shell\openas\command /f /t REG_EXPAND_SZ /d "\"C:\Program Files\Notepad++\notepad++.exe\" \"%%1\""
+
+REM ************* Open Extension less files in Notepad++ ***********
+REG ADD HKEY_CLASSES_ROOT\. /f /t REG_SZ /d txtfile
