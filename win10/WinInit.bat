1b3bf670d255701a190cb7ab7e81db5099979e9f
Morten Kristoffer Siljuberg <MSiljuberg@slb.com>
2020-10-27 04:54:26 +0100

    updates

--- win10/WinInit.bat
+++ win10/WinInit.bat
@@ -1,5 +1,7 @@
-reg add "HKEY_CLASSES_ROOT\Unknown\shell\openas\command" /v "ASDF" /t DWORD /d "TEST" /f
 
 setx _MSCRIPT "%userprofile%\.cfg\scripts"
-
 rem %AppData%\Microsoft\Windows\Start Menu\Programs\Startup
+
+
+REM ************* Set default text editor across Windows ***********
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\txtfile\shell\open\command"  /f /t REG_EXPAND_SZ /d "\"C:\Program Files\Notepad++\notepad++.exe\" \"%%1\""
