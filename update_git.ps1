ed8094af8d2cbb63a2a6b994d79cf13959ae27e9
Morten Kristoffer Siljuberg <MSiljuberg@slb.com>
2021-03-05 16:30:17 +0100

    updates

--- update_git.ps1
+++ update_git.ps1
@@ -1,6 +1,6 @@
 git add .
-git reset --np++\config.xml
-git reset --FileWatcherUtilities/fwatcher.log
+git reset np++\config.xml
+git reset FileWatcherUtilities/fwatcher.log
 
 git commit -m updates
 git push
